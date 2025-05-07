f5c263177483b12b2b89f276ad63dde4c2828704
yeonnan <145152442+yeonnan@users.noreply.github.com>
2025-05-07 22:36:12 +0900

    예외 발생시키기

---
+++ 11_python.py
@@ -0,0 +1,14 @@
+ItemsInCart = 0
+
+if ItemsInCart != 2:
+    # raise Exception('실패실패')       # Exception: 실패실패
+    pass
+
+
+# 조건이 충족하지 않을 경우 실패하게 되는 방법
+'''
+assert 
+조건을 예상하는 곳에서 사용할 수 있는 파이썬 메서드
+조건은 항상 True에 해당해야 하며, 조건이 True가 아니라면 테스트 실패
+'''
+assert(ItemsInCart==2)          # AssertionError
