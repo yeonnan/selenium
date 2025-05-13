9510f941be4547fda49729306e767b29666ef757
yeonnan <145152442+yeonnan@users.noreply.github.com>
2025-05-13 23:50:25 +0900

    html 요소 속성 검사

---
+++ selenium/locators.py
@@ -0,0 +1,25 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+
+service_obj = Service('/Users/dayeonahn/Downloads/chromedriver-mac-arm64/chromedriver') 
+driver = webdriver.Chrome(service=service_obj)
+
+driver.get('https://github.com/login')
+
+
+# Selenium은 ID, XPath, CSS, Classname, name 속성, LinkText 같은 로케이터를 지원
+# 로케이터 얻는법 : 브라우저 - 개발자 도구 -> 섹션 찾아서 class 속성
+
+# selenium에게 페이지에 있는 요소 중에 name 로케이터로 입력창을 찾으라고 요청
+# 코드를 보고 페이지를 스캔해서 name속성이 login인 요소를 찾게된다.
+driver.find_element(By.NAME, 'login').send_keys('hello@gmail.com')
+driver.find_element(By.ID, 'password').send_keys('test1234!')
+driver.find_element(By.NAME, 'commit').click()
+
+# name이나 id 같은 것들은 개발자들이 정의해둬야 사용 가능하다.
+# 그런데 어떤 요소, 속성, 정의든 XPath나 CSS는 페이지의 아무 요소에나 활용 가능하다.
+
+time.sleep(10)
