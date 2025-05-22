f1e8690231bd4832fbce2981bde730ad66235907
yeonnan <145152442+yeonnan@users.noreply.github.com>
2025-05-22 22:47:20 +0900

    value 속성을 가져와 브라우저 동적 텍스트의 유효성 검증하기

--- selenium/findElementsTest.py
+++ selenium/findElementsTest.py
@@ -19,5 +19,15 @@ for country in countries:
         country.click() 
         break
 
+'''
+.text를 쓸 때 자동화를 통해 스크립트를 동적으로 업데이트하면 텍스트를 못가져온다
+-> 웹 애플리케이션 내부에 텍스트가 미리 있어야 가능
+=> 변경 사항이 있거나 스크립트를 통해 가져오는 텍스트는 .text로 가져올 수 없다.
+'''
+# print(driver.find_element(By.ID, 'autosuggest').text)
+
+# 잘 들어왔는지 확인하기
+assert driver.find_element(By.ID, 'autosuggest').get_attribute('value') == 'Korea, Republic of'
+
 time.sleep(2)       # 클릭 동작 후, 결과가 반영되는 걸 잠깐 눈으로 볼 수 있게 대기하는 용도
 driver.quit()
