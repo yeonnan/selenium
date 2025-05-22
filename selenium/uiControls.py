f3720efd8a28dac851855d91880a4c601556e6ff
yeonnan <145152442+yeonnan@users.noreply.github.com>
2025-05-22 22:53:10 +0900

    체크박스 동적으로 처리하기

---
+++ selenium/uiControls.py
@@ -0,0 +1,24 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+
+service_obj = Service('/Users/dayeonahn/Downloads/chromedriver-mac-arm64/chromedriver') 
+driver = webdriver.Chrome(service=service_obj)
+
+driver.get('https://rahulshettyacademy.com/AutomationPractice/')
+checkboxes = driver.find_elements(By.XPATH, '//input[@type="checkbox"]')
+
+print(len(checkboxes))
+
+# 리스트를 순회하며 반복할 때마다 하나의 웹 요소를 가져와 변수에 저장
+for checkbox in checkboxes:
+    # 가져온 변수에서 ID, value, name 속성을 확인 -> get_attribute()
+    if checkbox.get_attribute('value') == 'option2':
+        checkbox.click()
+        assert checkbox.is_selected()      # 해당 체크박스가 선택되었는지 여부 알려주는 메서드
+        break
+
+time.sleep(3)
+driver.quit()       # selenium이 띄운 모든 브라우저 창 완전히 종료, webdriver 세션도 메모리에서 완전히 정리
