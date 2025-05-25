245e9cd3f0513851d9430ac009ed6324e7e859c3
yeonnan <145152442+yeonnan@users.noreply.github.com>
2025-05-25 22:29:31 +0900

    tc

--- selenium/waitDemo.py
+++ selenium/waitDemo.py
@@ -19,6 +19,16 @@ results = driver.find_elements(By.XPATH, '//div[@class="products"]/div')
 count = len(results)
 assert count > 0
 
+for result in results:
+    result.find_element(By.XPATH, 'div/button').click()
+
+driver.find_element(By.CSS_SELECTOR, 'img[alt="Cart"]').click()
+driver.find_element(By.XPATH, '//button[text()="PROCEED TO CHECKOUT"]').click()
+driver.find_element(By.CSS_SELECTOR, '.promoCode').send_keys('rahulshettyacademy')
+driver.find_element(By.CSS_SELECTOR, '.promoBtn').click()
+time.sleep(5)
+print(driver.find_element(By.CLASS_NAME, 'promoInfo').text)
+
 
 time.sleep(3)
 driver.quit()  
