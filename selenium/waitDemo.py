fe327df896ef4544197e166dee3daea63e5c18fd
yeonnan <145152442+yeonnan@users.noreply.github.com>
2025-05-26 22:54:57 +0900

    암시적 대기

--- selenium/waitDemo.py
+++ selenium/waitDemo.py
@@ -7,6 +7,12 @@ from selenium.webdriver.common.by import By
 service_obj = Service('/Users/dayeonahn/Downloads/chromedriver-mac-arm64/chromedriver') 
 driver = webdriver.Chrome(service=service_obj)
 
+# 암시적 대기 : 스크립트 가장 위에서 한번만 선언하면 모든 요소를 찾을 때마다 최대 지정한 시간만큼 기다려준다.
+driver.implicitly_wait(5)
+
+# 페이지에 아무 요소도 뜨지 않으면 뜰 때까지 최대 5초 대기. 찾을때까지 맹목적으로 대기하는게 아닌 최대 대기시간
+# sleep으로 대기하는 경우는 무조건 해당 시간을 다 기다리는데, 암시적 대기는 전반적인 대기시간을 설정하고, 그 안에 요소가 표시되면 다음 단계를 진행
+
 driver.get('https://rahulshettyacademy.com/seleniumPractise/#/')
 
 # 태그 이름 속성이 value인 css 사용하는 방법 -> 클래스 이름으로 css 사용 가능
@@ -24,9 +30,9 @@ for result in results:
 
 driver.find_element(By.CSS_SELECTOR, 'img[alt="Cart"]').click()
 driver.find_element(By.XPATH, '//button[text()="PROCEED TO CHECKOUT"]').click()
+
 driver.find_element(By.CSS_SELECTOR, '.promoCode').send_keys('rahulshettyacademy')
 driver.find_element(By.CSS_SELECTOR, '.promoBtn').click()
-time.sleep(5)
 print(driver.find_element(By.CLASS_NAME, 'promoInfo').text)
 
 
