42b89b7cd9d92739e1788904ed5d5c821e8f7f09
yeonnan <145152442+yeonnan@users.noreply.github.com>
2025-06-03 19:35:14 +0900

    제품 추가 로직

--- selenium/e2eTest.py
+++ selenium/e2eTest.py
@@ -10,7 +10,14 @@ driver.implicitly_wait(4)
 
 driver.get('https://rahulshettyacademy.com/angularpractice/')
 driver.find_element(By.CSS_SELECTOR, 'a[href*="shop"]').click()
+products = driver.find_elements(By.XPATH, '//div[@class="card h-100"]')
 
+for product in products:
+    productName = product.find_element(By.XPATH, 'div/h4/a').text
+    if productName == 'Blackberry':
+        product.find_element(By.XPATH, 'div/button').click()
+
+driver.find_element(By.CSS_SELECTOR, 'a[class*="btn-primary"]').click()
 
 time.sleep(3)
 driver.quit()  
