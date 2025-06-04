4d9f447af75b1874e7c75c5d825c4f5e61a7f71e
yeonnan <145152442+yeonnan@users.noreply.github.com>
2025-06-04 20:53:57 +0900

    드롭다운 선택

--- selenium/e2eTest.py
+++ selenium/e2eTest.py
@@ -2,6 +2,8 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
 
 
 service_obj = Service('/Users/dayeonahn/Downloads/chromedriver-mac-arm64/chromedriver') 
@@ -18,6 +20,17 @@ for product in products:
         product.find_element(By.XPATH, 'div/button').click()
 
 driver.find_element(By.CSS_SELECTOR, 'a[class*="btn-primary"]').click()
+driver.find_element(By.XPATH, '//button[@class="btn btn-success"]').click()
+driver.find_element(By.ID, 'country').send_keys('mol')
+wait = WebDriverWait(driver, 10)
+wait.until(expected_conditions.presence_of_element_located((By.LINK_TEXT, 'Moldova')))
+driver.find_element(By.LINK_TEXT, 'Moldova').click()
+driver.find_element(By.XPATH, '//div[@class="checkbox checkbox-primary"]').click()
+driver.find_element(By.CSS_SELECTOR, '[type="submit"]').click()
+successText = driver.find_element(By.CLASS_NAME, 'alert-success').text
+
+assert 'Success! Thank you!' in successText
+driver.close()
 
 time.sleep(3)
 driver.quit()  
