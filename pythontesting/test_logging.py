fdac3d8243474dd08255a50be568f3ecad49c3c6
yeonnan <145152442+yeonnan@users.noreply.github.com>
2025-06-14 20:01:01 +0900

    로깅 유틸리티

--- pythontesting/test_logging.py
+++ pythontesting/test_logging.py
@@ -1,13 +1,19 @@
 import logging
 
+def test_logging():
+    logger = logging.getLogger(__name__)
 
-logger = logging.getLogger(__name__)
+    fileHandler = logging.FileHandler('logfile.log')
+    # s를 붙여야 문자열로 취급되어 최종적으로 모두 연결 가능
+    formatter = logging.Formatter('%(asctime)s : %(levelname)s : %(name)s : %(message)s')
+    fileHandler.setFormatter(formatter)
 
-fileHandler = logging.FileHandler('logfile.log')
-logger.addHandler(fileHandler)     # filehandler object : logger가 어떤 파일을 어떤 형식으로 출력할 건지 알려주기 위한 것
+    logger.addHandler(fileHandler)     # filehandler object : logger가 어떤 파일을 어떤 형식으로 출력할 건지 알려주기 위한 것
 
-logger.debug('디버그 실행됨')
-logger.info('정보 구문')
-logger.warning('경고경고')
-logger.error('오류 발생')
-logger.critical('심각한 문제')
+    logger.setLevel(logging.DEBUG)
+    logger.debug('디버그 실행됨')
+    logger.info('정보 구문')
+    logger.debug('디버그 실행됨')
+    logger.warning('경고경고')
+    logger.error('오류 발생')
+    logger.critical('심각한 문제')
